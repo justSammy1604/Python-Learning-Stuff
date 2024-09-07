4b1635de086d223a649a167076ae714aeca72fde
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-09-07 23:56:34 +0530

    Update cnn.py

--- Pytorch/cnn.py
+++ Pytorch/cnn.py
@@ -6,6 +6,7 @@ import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
+// Adding GPU usage 
 device = torch.device('cuda:0')
 
 # 0 prepare data
