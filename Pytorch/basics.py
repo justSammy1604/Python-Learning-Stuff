27ccc341a62b33b91da090d95694ca8bebe20221
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-10-06 14:20:52 +0530

    Update basics.py

--- Pytorch/basics.py
+++ Pytorch/basics.py
@@ -3,7 +3,7 @@
 import torch as tor 
 device = tor.device('cuda:0')
 X = tor.tensor([1,2,3,4,5], dtype=tor.float32)
-Y = tor.tensor([2,4,6,8,10], dtype=tor.float32)
+Y = tor.tensor([2,4,6,8,10], dtype=tor.float32) 
 
 w = tor.tensor(0.0, dtype=tor.float32, requires_grad=True) 
  
