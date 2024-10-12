d894af8cd23139dd020de6ea29fd001bc5fe8e91
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-10-12 17:46:28 +0530

    Update eye_track.py

--- OpenCV/eye_track.py
+++ OpenCV/eye_track.py
@@ -1,6 +1,6 @@
 import cv2
 import tkinter as tk
-from tkinter import messagebox 
+from tkinter import messagebox  
 
 # Load the required trained XML classifiers for face and eyes
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
